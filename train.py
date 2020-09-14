d9fdfc6cc97fbe2baa24cd2e3980f9ca03504c14
Mark Ma <mzma@usc.edu>
2020-09-14 14:37:43 -0700

    added action sequences for all tetris pieces

--- train.py
+++ train.py
@@ -16,15 +16,85 @@ TRAIN_EPISODES = 1000
 # for shape names see https://www.quora.com/What-are-the-different-blocks-in-Tetris-called-Is-there-a-specific-name-for-each-block
 # TODO: generate all combinations of action sequences for each shape
 ACTIONS = dict()
-ACTIONS['O'] = [['left']*4, ['left']*3, ['left']*2, ['left'], ['no move'], ['right']*4, ['right']*3, ['right']*2, ['right']]
-ACTIONS['I'] = []
-ACTIONS['J'] = []
-ACTIONS['L'] = []
-ACTIONS['S'] = []
-ACTIONS['T'] = []
-ACTIONS['Z'] = []
+ACTIONS['O'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 4, ['right'] * 3, ['right'] * 2, ['right']
+                ]
+ACTIONS['I'] = [['left'] * 5, ['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 4, ['right'] * 3, ['right'] * 2, ['right'],
+                ['rotate'] + ['left'] * 5, ['rotate'] + ['left'] * 4, ['rotate'] + ['left'] * 3,
+                ['rotate'] + ['left'] * 2, ['rotate'] + ['left'],
+                ['rotate'],
+                ['rotate'] + ['right']
+                ]
+ACTIONS['L'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 4, ['right'] * 3, ['right'] * 2, ['right'],
+                ['rotate'] + ['left'] * 4, ['rotate'] + ['left'] * 3, ['rotate'] + ['left'] * 2, ['rotate'] + ['left'],
+                ['rotate'],
+                ['rotate'] + ['right'] * 3, ['rotate'] + ['right'] * 2, ['rotate'] + ['right'],
+                ['rotate'] * 2 + ['left'] * 4, ['rotate'] * 2 + ['left'] * 3, ['rotate'] * 2 + ['left'] * 2,
+                ['rotate'] * 2 + ['left'],
+                ['rotate'] * 2,
+                ['rotate'] * 2 + ['right'] * 4, ['rotate'] * 2 + ['right'] * 3, ['rotate'] * 2 + ['right'] * 2,
+                ['rotate'] * 2 + ['right'],
+                ['rotate'] * 3 + ['left'] * 4, ['rotate'] * 3 + ['left'] * 3, ['rotate'] * 3 + ['left'] * 2,
+                ['rotate'] * 3 + ['left'],
+                ['rotate'] * 3,
+                ['rotate'] * 3 + ['right'] * 3, ['rotate'] * 3 + ['right'] * 2, ['rotate'] * 3 + ['right']
+                ]
+ACTIONS['J'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 4, ['right'] * 3, ['right'] * 2, ['right'],
+                ['rotate'] + ['left'] * 4, ['rotate'] + ['left'] * 3, ['rotate'] + ['left'] * 2, ['rotate'] + ['left'],
+                ['rotate'],
+                ['rotate'] + ['right'] * 3, ['rotate'] + ['right'] * 2, ['rotate'] + ['right'],
+                ['rotate'] * 2 + ['left'] * 4, ['rotate'] * 2 + ['left'] * 3, ['rotate'] * 2 + ['left'] * 2,
+                ['rotate'] * 2 + ['left'],
+                ['rotate'] * 2,
+                ['rotate'] * 2 + ['right'] * 4, ['rotate'] * 2 + ['right'] * 3, ['rotate'] * 2 + ['right'] * 2,
+                ['rotate'] * 2 + ['right'],
+                ['rotate'] * 3 + ['left'] * 4, ['rotate'] * 3 + ['left'] * 3, ['rotate'] * 3 + ['left'] * 2,
+                ['rotate'] * 3 + ['left'],
+                ['rotate'] * 3,
+                ['rotate'] * 3 + ['right'] * 3, ['rotate'] * 3 + ['right'] * 2, ['rotate'] * 3 + ['right']
+                ]
+ACTIONS['S'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 3, ['right'] * 2, ['right'],
+                ['rotate'] + ['left'] * 4, ['rotate'] + ['left'] * 3, ['rotate'] + ['left'] * 2, ['rotate'] + ['left'],
+                ['rotate'],
+                ['rotate'] + ['right'] * 4, ['rotate'] + ['right'] * 3, ['rotate'] + ['right'] * 2,
+                ['rotate'] + ['right'],
+                ]
+ACTIONS['T'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 3, ['right'] * 2, ['right'],
+                ['rotate'] + ['left'] * 4, ['rotate'] + ['left'] * 3, ['rotate'] + ['left'] * 2, ['rotate'] + ['left'],
+                ['rotate'],
+                ['rotate'] + ['right'] * 4, ['rotate'] + ['right'] * 3, ['rotate'] + ['right'] * 2,
+                ['rotate'] + ['right'],
+                ['rotate'] * 2 + ['left'] * 4, ['rotate'] * 2 + ['left'] * 3, ['rotate'] * 2 + ['left'] * 2,
+                ['rotate'] * 2 + ['left'],
+                ['rotate'] * 2,
+                ['rotate'] * 2 + ['right'] * 3, ['rotate'] * 2 + ['right'] * 2, ['rotate'] * 2 + ['right'],
+                ['rotate'] * 3 + ['left'] * 4, ['rotate'] * 3 + ['left'] * 3, ['rotate'] * 3 + ['left'] * 2,
+                ['rotate'] * 3 + ['left'],
+                ['rotate'] * 3,
+                ['rotate'] * 3 + ['right'] * 4, ['rotate'] * 3 + ['right'] * 3, ['rotate'] * 3 + ['right'] * 2,
+                ['rotate'] * 3 + ['right']
+                ]
+ACTIONS['Z'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
+                [],
+                ['right'] * 3, ['right'] * 2, ['right'],
+                ['rotate'] + ['left'] * 4, ['rotate'] + ['left'] * 3, ['rotate'] + ['left'] * 2, ['rotate'] + ['left'],
+                ['rotate'],
+                ['rotate'] + ['right'] * 4, ['rotate'] + ['right'] * 3, ['rotate'] + ['right'] * 2,
+                ['rotate'] + ['right'],
+                ]
 
-SHAPES = ['O','I','J','L','S','T','Z']
+SHAPES = ['O', 'I', 'J', 'L', 'S', 'T', 'Z']
 
 # all possible shapes
 
@@ -35,19 +105,18 @@ SHAPES = ['O','I','J','L','S','T','Z']
 # at = [0,1,2,3,4,5] = [left, right, turn1, turn2, turn3, no move']
 # Q_values
 Q_values = dict()
-Q_values['O'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['O'])))
-Q_values['I'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['I'])))
-Q_values['J'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['J'])))
-Q_values['L'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['L'])))
-Q_values['S'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['S'])))
-Q_values['T'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['T'])))
-Q_values['Z'] = np.zeros((5,5,5,5,5,5,5,5,5,5,len(ACTIONS['J'])))
-
-
+Q_values['O'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['O'])))
+Q_values['I'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['I'])))
+Q_values['J'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['J'])))
+Q_values['L'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['L'])))
+Q_values['S'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['S'])))
+Q_values['T'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['T'])))
+Q_values['Z'] = np.zeros((5, 5, 5, 5, 5, 5, 5, 5, 5, 5, len(ACTIONS['J'])))
 
 # test matrix
-test_mat = np.array(pd.read_csv('testData/test_mat.csv', header = None))
-terminal_mat = np.array(pd.read_csv('testData/terminal_mat.csv', header = None))
+test_mat = np.array(pd.read_csv('testData/test_mat.csv', header=None))
+terminal_mat = np.array(pd.read_csv('testData/terminal_mat.csv', header=None))
+
 
 # function definitions
 def is_terminal_state(st):
@@ -60,6 +129,7 @@ def is_terminal_state(st):
         return True
     else:
         return False
+
 
 def encode_state(st):
     """
@@ -77,7 +147,7 @@ def encode_state(st):
         highest_in_column = 0
         for j, _ in enumerate(col):
             if _ != 0:
-                highest_in_column = j+1
+                highest_in_column = j + 1
         reduced_state.append(highest_in_column)
         max_height = max(max_height, highest_in_column)
 
@@ -85,6 +155,7 @@ def encode_state(st):
     reduced_state = [max(x - baseline, 0) for x in reduced_state]
 
     return reduced_state
+
 
 def get_next_state(st, shape, action_index):
     """
@@ -99,14 +170,16 @@ def get_next_state(st, shape, action_index):
     # TODO: update state based on action_index and shape
     return new_st
 
+
 def get_reward(old_board, new_board):
     # TODO: calculate reward based on difference between old and new boards
     reward = 0
     return reward
 
+
 def get_new_random_shape():
     return np.random.choice(SHAPES)
-    
+
 
 def get_next_action(reduced_state, shape):
     if np.random.random() < EPSILON:
@@ -115,6 +188,7 @@ def get_next_action(reduced_state, shape):
     else:
         # return a random action
         return np.random.randint(len(ACTIONS[shape]))
+
 
 def train():
     for episode in range(TRAIN_EPISODES):
@@ -143,14 +217,3 @@ def train():
 
             # transition into new st
             st = new_st
-
-
-
-
-
-
-
-
-
-
-
