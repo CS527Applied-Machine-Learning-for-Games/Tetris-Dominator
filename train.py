692d47d2c5df3a9ba5a09ddce19829d5e65e2ca5
Mark Ma <mzma@usc.edu>
2020-09-17 18:24:51 -0700

    completed function get_next_state(st, shape, action_index)

--- train.py
+++ train.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 from collections import Counter
+from copy import deepcopy
+
 # constant definitions
 EPSILON = 0.9
 GAMMA = 0.9
@@ -96,14 +98,13 @@ ACTIONS['Z'] = [['left'] * 4, ['left'] * 3, ['left'] * 2, ['left'],
 SHAPES = ['O', 'I', 'J', 'L', 'S', 'T', 'Z']
 
 SHAPE_STARTING_COORDS = dict()
-SHAPE_STARTING_COORDS['O'] = [(19,4), (19,5), (18,4), (18,5)]
-SHAPE_STARTING_COORDS['I'] = [(19,5), (18,5), (17,5), (16,5)]
-SHAPE_STARTING_COORDS['L'] = [(19,4), (18,4), (17,4), (17,5)]
-SHAPE_STARTING_COORDS['J'] = [(17,4), (19,5), (18,4), (17,5)]
-SHAPE_STARTING_COORDS['S'] = [(18,4), (19,5), (18,5), (19,6)]
-SHAPE_STARTING_COORDS['Z'] = [(19,4), (19,5), (18,5), (18,6)]
-SHAPE_STARTING_COORDS['T'] = [(19,4), (19,5), (18,5), (19,6)]
-
+SHAPE_STARTING_COORDS['O'] = [(19, 4), (19, 5), (18, 4), (18, 5)]
+SHAPE_STARTING_COORDS['I'] = [(19, 5), (18, 5), (17, 5), (16, 5)]
+SHAPE_STARTING_COORDS['L'] = [(19, 4), (18, 4), (17, 4), (17, 5)]
+SHAPE_STARTING_COORDS['J'] = [(17, 4), (19, 5), (18, 4), (17, 5)]
+SHAPE_STARTING_COORDS['S'] = [(18, 4), (19, 5), (18, 5), (19, 6)]
+SHAPE_STARTING_COORDS['Z'] = [(19, 4), (19, 5), (18, 5), (18, 6)]
+SHAPE_STARTING_COORDS['T'] = [(19, 4), (19, 5), (18, 5), (19, 6)]
 
 # all possible shapes
 
@@ -165,91 +166,129 @@ def encode_state(st):
 
     return reduced_state
 
+
 def get_rotated_coordinates(shape, n):
     """
     n: number of rotations
     """
     if n == 0:
-        return shape
+        return deepcopy(SHAPE_STARTING_COORDS[shape])
     elif shape == 'O':
-        return shape
+        return deepcopy(SHAPE_STARTING_COORDS[shape])
     elif shape == 'I':
         if n == 1:
-            return [(19,4), (19,5), (19,6), (19,7)]
+            return [(19, 4), (19, 5), (19, 6), (19, 7)]
         else:
             raise Exception
     elif shape == 'L':
         if n == 1:
-            return [(18,4), (18,5), (19,6), (18,6)]
+            return [(18, 4), (18, 5), (19, 6), (18, 6)]
         elif n == 2:
-            return [(19,4), (19,5), (18,5), (17,5)]
+            return [(19, 4), (19, 5), (18, 5), (17, 5)]
         elif n == 3:
-            return [(19,4), (18,4), (19,5), (19,6)]
+            return [(19, 4), (18, 4), (19, 5), (19, 6)]
         else:
             raise Exception
     elif shape == 'J':
         if n == 1:
-            return [(19,4), (19,5), (19,6), (18,6)]
+            return [(19, 4), (19, 5), (19, 6), (18, 6)]
         elif n == 2:
-            return [(19,4), (18,4), (17,4), (19,5)]
+            return [(19, 4), (18, 4), (17, 4), (19, 5)]
         elif n == 3:
-            return [(19,4), (18,4), (18,5), (18,6)]
+            return [(19, 4), (18, 4), (18, 5), (18, 6)]
         else:
             raise Exception
     elif shape == 'S':
         if n == 1:
-            return [(19,4), (18,4), (18,5), (17,5)]
+            return [(19, 4), (18, 4), (18, 5), (17, 5)]
         else:
             raise Exception
     elif shape == 'Z':
         if n == 1:
-            return [(18,4), (17,4), (19,5), (18,5)]
+            return [(18, 4), (17, 4), (19, 5), (18, 5)]
         else:
             raise Exception
     elif shape == 'T':
         if n == 1:
-            return [(19,4), (18,4), (17,4), (18,5)]
+            return [(19, 4), (18, 4), (17, 4), (18, 5)]
         elif n == 2:
-            return [(18,4), (19,5), (18,5), (18,6)]
+            return [(18, 4), (19, 5), (18, 5), (18, 6)]
         elif n == 3:
-            return [(18,4), (19,5), (18,5), (17,5)]
+            return [(18, 4), (19, 5), (18, 5), (17, 5)]
         else:
             raise Exception
     else:
         raise Exception
-            
-    
+
 
 def get_terminal_position_before_drop(shape, action):
-
     """return coordinates of <shape> after it has moved through the sequence of <action>"""
     # create a counter for number of rotations and left/right moves
     action_counter = Counter(action)
     n_left = action_counter['left']
     n_rotate = action_counter['rotate']
     n_right = action_counter['right']
-    
+
     terminal_position_before_drop = get_rotated_coordinates(shape, n_rotate)
     if n_left:
         # move left n_left times
         for i in range(len(terminal_position_before_drop)):
-            (x,y) = terminal_position_before_drop[i]
+            (x, y) = terminal_position_before_drop[i]
             terminal_position_before_drop[i] = (x, y - n_left)
         # move right n_right times
     if n_right:
         for i in range(len(terminal_position_before_drop)):
-            (x,y) = terminal_position_before_drop[i]
+            (x, y) = terminal_position_before_drop[i]
             terminal_position_before_drop[i] = (x, y + n_right)
 
     return terminal_position_before_drop
 
-def get_next_state(st, terminal_position_before_drop):
+
+def get_next_state(st, shape, action_index):
     """
     gets next state based on current state, and terminal configuration of tetris piece before it is dropped
     """
-    old_st = st
-    new_st = st
-    # TODO
+    # make a deepcopy of new state so that we still have the configurations of the old state to compare
+    new_st = deepcopy(st)
+
+    action = ACTIONS[shape][action_index]
+    terminal_position_before_drop = get_terminal_position_before_drop(shape, action)
+
+    # <col:lowest_coord_in_col> for shape
+    shape_bottom_coords = dict()
+    for row, col in terminal_position_before_drop:
+        try:
+            shape_bottom_coords[col] = min(shape_bottom_coords[col], row)
+        except KeyError:
+            shape_bottom_coords[col] = row
+
+    # <col:highest_coord_in_col> for board
+    board_top_height = dict()
+    for col in range(len(st[0])):
+        if np.where(st[:, col] == 1)[0].size == 0:
+            board_top_height[col] = 0
+        else:
+            board_top_height[col] = len(st) - 1 - np.where(st[:, col] == 1)[0][0]
+
+    # determine minimum gap between shape and current tetris board, and corresponding column
+    min_gap = 20
+    for col in shape_bottom_coords.keys():
+        if shape_bottom_coords[col] - board_top_height[col] < min_gap:
+            min_gap = shape_bottom_coords[col] - board_top_height[col]
+
+    # bring all columns of tetriminoe down by min_gap
+    for i, j in terminal_position_before_drop:
+        # convert to vertical coordinates for st
+        i = 19 - i
+        # fill board
+        new_st[i + min_gap - 1][j] = 1
+
+    # detect any complete lines and cancel them if any
+    new_st = new_st[np.where(np.count_nonzero(new_st, axis=1) < 10)]
+
+    if len(new_st) != len(st):
+        lines_cancelled = len(st) - len(new_st)
+        new_st = np.insert(new_st, 0, [np.zeros(10)]*lines_cancelled, 0)
     return new_st
 
 
@@ -257,6 +296,7 @@ def get_reward(old_board, new_board):
     # TODO: calculate reward based on difference between old and new boards
     reward = 0
     return reward
+
 
 def get_new_random_shape():
     return np.random.choice(SHAPES)
